0896fb55def5799e87653dfdc987a124559ef6fc
Waleed <wmm3636@rit.edu>
2018-12-22 16:56:25 -0500

    added firebase config

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 
+class App extends React.Component {
+  ComponentWillMount(){
+    const config = {
+      apiKey: "AIzaSyB1YiEfMKJ_QRONLdANaHDh0UoZ_DuJP3U",
+      authDomain: "firstproject-5e368.firebaseapp.com",
+      databaseURL: "https://firstproject-5e368.firebaseio.com",
+      projectId: "firstproject-5e368",
+      storageBucket: "firstproject-5e368.appspot.com",
+      messagingSenderId: "435468636684"
+    };
+    firebase.initializeApp(config);
+  }
+}
 export default class App extends React.Component {
   render() {
     return (
